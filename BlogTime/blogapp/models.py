c8174654bbec5b505c619c015270f5897eb3de46
nduwal3 <duwalniru0@gmail.com>
2020-07-14 23:18:21 +0545

    created and migrated models

--- BlogTime/blogapp/models.py
+++ BlogTime/blogapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    username = models.CharField(max_length = 250)
+    email = models.EmailField()
+    password = models.CharField(max_length= 100)
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 250)
+    body = models.TextField()
+    author = models.OneToOneField(Author, on_delete = models.PROTECT, related_name = "author")
+    created_date = models.DateTimeField(auto_now = False, auto_now_add = False)
+    updated = models.DateTimeField(auto_now = False, auto_now_add = False)
+    is_published = models.BooleanField(default = False)
+
