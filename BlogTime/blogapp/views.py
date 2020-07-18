f6ef3383143362b9690c84b78774ae1c5f187b40
Nduwal3 <duwalniru0@gmail.com>
2020-07-18 19:48:25 +0545

    added homepage and blogpage

--- BlogTime/blogapp/views.py
+++ BlogTime/blogapp/views.py
@@ -3,4 +3,20 @@ from .models import Blog
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    category_list = ['WellBeing', 'Sustainable Living', 'Furniture', 'Travel' , 'Design', "Technology"]
+    context={
+        'category_list': category_list
+    }
+
+    return render(request, 'home.html', context)
+    
+
+def blog_list(request):
+    blog_list = Blog.objects.all()
+    print(blog_list)
+    context={
+        'blog_list': blog_list
+    }
+
+    return render(request, 'blogpage.html', context)
+
